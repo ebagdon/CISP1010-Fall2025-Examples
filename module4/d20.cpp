8b6a35851857b48dbabe1d192d969910e700ebc4
gymmyp1 <40799820+gymmyp1@users.noreply.github.com>
2025-09-22 17:26:44 +0000

    separate regular d20 with d20 pity examples

--- module4/d20.cpp
+++ module4/d20.cpp
@@ -1,3 +1,7 @@
+/* Simulate rolling a d20. It's unfairly weighted and 
+has a 6% chance of rolling a 20.
+*/
+
 #include <iostream>
 #include <ctime>
 
@@ -15,23 +19,14 @@ int main() {
     
     while (choice != 'N') {
         chance = (double)rand()/RAND_MAX; //gives you a random number b/w 0.0 and 1.0
-        if(counter >= 10) {
-            cout << "You've hit pity." << endl;
-            if(chance <= 0.12) {
-                roll = 20;
-            } else {
-                cout << "regular roll between 1-19" << endl;
-                roll = 1 + rand() % 19; //roll w/ even chance of 1-19
-            }
+        if (chance <= 0.06) { //if I get 6%, roll 20 (yay)
+            cout << "6% chance of 20." << endl;
+            roll = 20;
         } else {
-            if (chance <= 0.06) { //if I get 6%, roll 20 (yay)
-                cout << "6% chance of 20." << endl;
-                roll = 20;
-            } else {
-                cout << "regular roll between 1-19" << endl;
-                roll = 1 + rand() % 19; //roll w/ even chance of 1-19
-            }
+            cout << "regular roll between 1-19" << endl;
+            roll = 1 + rand() % 19; //roll w/ even chance of 1-19
         }
+
         counter++;
         cout << "Number of times rolled: " << counter << endl;
         cout << "Roll: " << roll << endl;
